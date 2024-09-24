d3f98ba96b6dbe15eedcd3718d933d5bd6b64412
cod3d <vsevolodkutsyn@proton.me>
2024-09-23 21:35:45 -0700

    week 1

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # it261
+
 Materials for the IT261 course at SCC
+
+Files are automatically synced to the ftp server on push
---
+++ website/index.php
@@ -0,0 +1,60 @@
+<?php
+$protocol = $_SERVER['PROTOCOL'] = isset($_SERVER['HTTPS']) && !empty($_SERVER['HTTPS']) ? 'https' : 'http';
+$url = $protocol . "://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home page</title>
+    <link rel="stylesheet" href="/it261/website/css/index.css">
+</head>
+
+<body>
+    <header>
+        <img src="/it261/website/images/logo.png" alt="logo image">
+        <nav>
+            <ul>
+                <li><a href="/it261/index.php">Home</a></li>
+                <li><a href="/it261/website/about.php">About</a></li>
+                <li><a href="/it261/website/daily.php">Daily</a></li>
+                <li><a href="/it261/website/contact.php">Project</a></li>
+                <li><a href="/it261/website/contact.php">Contact</a></li>
+                <li><a href="/it261/website/contact.php">Gallery</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <img src="/it261/website/images/hero.png" alt="hero image">
+        <div class="content-wrapper">
+            <div class="content">
+
+                <h1>Welcome</h1>
+                <p>This is a test website for IT261</p>
+
+                <article>
+                    <h2>PHP, aka the Hypertext Preprocessor</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime adipisci, totam sit vitae minus facere quis iste minima consectetur, nam aliquam nulla modi, voluptas dolorum numquam fuga suscipit dolor quod.</p>
+                </article>
+            </div>
+            <aside>
+                <h2>Aside</h2>
+                <p>Adipisicing elit. Maxime adipisci, totam sit vitae minus facere quis iste minima consectetur, nam aliquam nulla modi, voluptas dolorum numquam fuga suscipit dolor quod.</p>
+            </aside>
+        </div>
+    </main>
+    <footer>
+        <ul>
+            <li>Copyright &copy; 2024</li>
+            <li>All rights reserved</li>
+            <li><a href="/it261/">Web design by -_-</a></li>
+            <li><a href="https://validator.w3.org/?doc=<?= $url ?>">HTML Validation</a></li>
+            <li><a href="https://jigsaw.w3.org/css-validator/?uri=<?= $url ?>">CSS Validation</a></li>
+        </ul>
+    </footer>
+</body>
+
+</html>
